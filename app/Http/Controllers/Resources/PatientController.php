ec3e869d1f86f5d06c1bd3fc12dad6a2717aec5a
Syaeful <kunsyaeful@gmail.com>
2026-04-13 04:15:12 +0700

    feat(user): implement patient search

--- app/Http/Controllers/Resources/PatientController.php
+++ app/Http/Controllers/Resources/PatientController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\Resources;
 
 use App\Http\Controllers\Controller;
 use App\Models\Resources\Patient;
+use App\Queries\Resources\PatientQueryBuilder;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PatientController extends Controller
 {
-    public function index()
+    public function index(PatientQueryBuilder $query)
     {
         return Inertia::render('patient/Index', [
-            'patients' => Patient::paginate(10),
+            'patients' => $query->paginate(10),
         ]);
     }
 }
--- resources/js/pages/patient/Index.vue
+++ resources/js/pages/patient/Index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { index } from '@/actions/App/Http/Controllers/Resources/PatientController';
 import AppLayout from '@/layouts/AppLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
 import { BreadcrumbItem, Pagination } from '@/types';
 import { Patient } from '@/types/resources/patient';
 import DataTable from '@/components/ui/datatable/DataTable.vue';
 import { PatientColumn } from '@/pages/patient/columns/patient-column';
+import DataTableSearch from '@/components/ui/datatable/DataTableSearch.vue';
+import { useDebounceFn } from '@vueuse/core';
+import { computed } from 'vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -14,11 +17,24 @@ const breadcrumbs: BreadcrumbItem[] = [
     }
 ]
 
-defineProps<{
+const props = defineProps<{
     patients: Pagination<Patient>
 }>()
 
+const patients = computed(() => props.patients)
+
 const column = PatientColumn()
+
+const handleSearch = useDebounceFn((value: string) => {
+    router.get(index().url, {
+        filter: {
+            name: value
+        }
+    }, {
+        preserveState: true,
+        replace: true
+    })
+}, 500)
 </script>
 
 <template>
@@ -26,7 +42,11 @@ const column = PatientColumn()
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="p-8 flex flex-col gap-4">
             <h1 class="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight">Daftar Pasien</h1>
-            <DataTable :columns="column" :pagination="patients"/>
+            <DataTable :columns="column" :pagination="patients">
+                <template #toolbar>
+                    <DataTableSearch @input="handleSearch"/>
+                </template>
+            </DataTable>
         </div>
     </AppLayout>
 </template>

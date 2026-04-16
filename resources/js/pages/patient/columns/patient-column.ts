125c1e4fe2fbe2c62b800dcfe423e354b973e939
Syaeful <kunsyaeful@gmail.com>
2026-04-16 19:28:00 +0700

    feat(patient): add medical record number column

--- resources/js/pages/patient/columns/patient-column.ts
+++ resources/js/pages/patient/columns/patient-column.ts
@@ -3,6 +3,10 @@ import { ColumnDef } from "@tanstack/vue-table";
 
 export const PatientColumn = (): ColumnDef<Patient>[] => [
     {
+        accessorKey: 'medical_record_number',
+        header: 'Nomor Rekam Medis',
+    },
+    {
         accessorKey: 'name',
         header: 'Nama'
     },

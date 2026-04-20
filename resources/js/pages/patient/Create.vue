f45eb10c2a9a5cee4c5c14d4c8590cc9d807407b
Syaeful <kunsyaeful@gmail.com>
2026-04-20 19:19:00 +0700

    refactor(patient): enhance patient form

--- resources/js/pages/patient/Create.vue
+++ resources/js/pages/patient/Create.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { create, index } from '@/actions/App/Http/Controllers/Resources/PatientController';
 import AppLayout from '@/layouts/AppLayout.vue';
-import CreatePatientForm from '@/pages/patient/components/CreatePatientForm.vue';
+import PatientForm from '@/pages/patient/components/form/PatientForm.vue';
 import { store } from '@/routes/patient';
 import { CreatePatientSchema } from '@/schemas/patient';
 import { BreadcrumbItem } from '@/types';
@@ -10,30 +10,35 @@ import { z } from 'zod';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        'title': 'Pasien',
-        'href': index().url
+        title: 'Pasien',
+        href: index().url,
     },
     {
-        'title': 'Tambah Pasien',
-        'href': create().url
-    }
-]
+        title: 'Tambah Pasien',
+        href: create().url,
+    },
+];
 
 defineProps<{
-    medical_record_number: string
-}>()
+    medical_record_number: string;
+}>();
 
 const handleSubmit = (data: z.infer<typeof CreatePatientSchema>) => {
     router.post(store().url, data);
-}
+};
 </script>
 
 <template>
-    <Head title="Tambah Pasien"/>
+    <Head title="Tambah Pasien" />
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="p-8 flex flex-col gap-4">
+        <div class="flex flex-col gap-4 p-8">
             <h1 class="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight">Tambah Pasien</h1>
-            <CreatePatientForm @submit="handleSubmit" :medical_record_number="medical_record_number"/>
+            <PatientForm
+                @submit="handleSubmit"
+                form-id="create-patient"
+                submit-label="Tambah Pasien"
+                :medical_record_number="medical_record_number"
+            />
         </div>
     </AppLayout>
 </template>
--- resources/js/pages/patient/Edit.vue
+++ resources/js/pages/patient/Edit.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { edit, index } from '@/actions/App/Http/Controllers/Resources/PatientController';
 import AppLayout from '@/layouts/AppLayout.vue';
-import EditPatientForm from '@/pages/patient/components/EditPatientForm.vue';
+import PatientForm from '@/pages/patient/components/form/PatientForm.vue';
 import { CreatePatientSchema } from '@/schemas/patient';
 import { BreadcrumbItem } from '@/types';
 import { Patient } from '@/types/resources/patient';
-import { z } from 'zod';
 import { Head, router } from '@inertiajs/vue3';
+import { z } from 'zod';
 
 const props = defineProps<{
-    patient: Patient
-}>()
+    patient: Patient;
+}>();
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -21,24 +21,26 @@ const breadcrumbs: BreadcrumbItem[] = [
         title: 'Edit Pasien',
         href: edit({
             patient: props.patient.id,
-        }).url
-    }
-]
+        }).url,
+    },
+];
 
 const handleEdit = (val: z.infer<typeof CreatePatientSchema>) => {
-    router.patch(edit({
-        patient: props.patient.id
-    }).url, val)
-}
-
+    router.patch(
+        edit({
+            patient: props.patient.id,
+        }).url,
+        val,
+    );
+};
 </script>
 
 <template>
-    <Head title="Edit Pasien"/>
+    <Head title="Edit Pasien" />
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="p-8 flex flex-col gap-4">
-             <h1 class="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight">Edit Pasien</h1>
-            <EditPatientForm @submit="handleEdit" :patient="patient" />
+        <div class="flex flex-col gap-4 p-8">
+            <h1 class="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight">Edit Pasien</h1>
+            <PatientForm @submit="handleEdit" form-id="edit-patient" submit-label="Edit Pasien" :patient="patient" />
         </div>
     </AppLayout>
 </template>

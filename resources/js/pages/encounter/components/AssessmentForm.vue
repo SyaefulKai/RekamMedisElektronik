2088d23d42c109be755f615689eb78a1d2dd59bd
Syaeful <kunsyaeful@gmail.com>
2026-05-01 19:10:57 +0700

    feat(assessment): improve layout and formatting in AssessmentForm component

--- resources/js/pages/encounter/components/AssessmentForm.vue
+++ resources/js/pages/encounter/components/AssessmentForm.vue
@@ -5,15 +5,15 @@ import DiagnosisDialog from '@/pages/encounter/components/DiagnosisDialog.vue';
 import { Encounter } from '@/types/resources/encounter';
 import { inject, Ref } from 'vue';
 
-const encounter = inject<Ref<Encounter>>('encounter')
-
+const encounter = inject<Ref<Encounter>>('encounter');
 </script>
 
 <template>
     <div>
-        <div class="flex justify-end">
+        <div class="flex items-center justify-between mt-4">
+            <h4 class="scroll-m-20 text-xl font-semibold tracking-tight">Diagnosa</h4>
             <DiagnosisDialog />
         </div>
-        <DataTable :columns="Icd10Column" :pagination="encounter?.assessment?.assessment_diagnoses ?? []"/>
+        <DataTable :columns="Icd10Column" :pagination="encounter?.assessment?.assessment_diagnoses ?? []" />
     </div>
 </template>

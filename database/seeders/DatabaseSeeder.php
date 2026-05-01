c97a9099ddcd61c30c2eab3b45140a90c1b55b5a
Syaeful <kunsyaeful@gmail.com>
2026-05-01 18:45:29 +0700

    feat(seeder): add ic10seeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([
             PermissionSeeder::class,
-            RoleSeeder::class
+            RoleSeeder::class,
+            Icd10Seeder::class,
         ]);
 
         $user = User::factory()->create([
